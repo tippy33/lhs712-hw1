0125c6a9fb31fe9293249d36380cfe80b151296b
Chris Hong <tippy33@naver.com>
2023-02-02 00:26:03 -0500

    add normalize_date, write functions

--- regex.py
+++ regex.py
@@ -1,33 +1,51 @@
 import re
 
+def normalize_date(type, date):
+  if type == "year" and len(str(date)) == 2:
+    return '19'+str(date)
+  elif type != "year" and len(str(date)) == 1:
+      return '0'+str(date)
+  else:
+    return date
+
+def write(line_number, date):
+  # print(type(date))
+  f = open('LHS712-Assg1-hngchris.txt', 'a')
+  f.write(line_number+'\t'+date+'\n')
+  f.close()
+
 def parse_date():
   file = open('dates.txt', 'r')
   lines = file.readlines()
   for l in lines:
     line_number = re.search(r'^[0-9]+', l)  # find line number
+    print(line_number[0])
     if line_number is None:
       continue   # no need to loop if line number is missing
     date = None
-    date_pattern = [r'([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{1,2})']
+    date_pattern = [r'([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{2,4})', 
+                    r'([0-9]{1,2})\/([0-9]{2,4})',
+                    r'([0-9]{4})']
     for p in date_pattern:
       date_group = re.search(p, l)
-      print(date_group)
+      # print(date_group.group())
       if date_group is not None:
-        # print(date_group[0])
-        # print(date_group[1])
-        # print(date_group[2])
-        ######
-        month = date_group[1]
-        if len(str(month)) == 1:
-          month = '0'+str(month)
-        day = date_group[2]
-        if len(str(day)) == 1:
-          day = '0'+str(day)
-        year = date_group[3]
-        if len(str(year)) == 2:
-          year = '19'+str(year)
-        date = year+'-'+month+'-'+day
-        print(date)
+        # print(len(date_group.groups()))
+        if len(date_group.groups()) == 3:
+          month = normalize_date('month', date_group[1])
+          day = normalize_date('day', date_group[2])
+          year = normalize_date('year', date_group[3])
+        elif len(date_group.groups()) == 2:
+          day = '01'
+          month = normalize_date('month', date_group[1])
+          year = normalize_date('year', date_group[2])
+        elif len(date_group.groups()) == 1:
+          day = '01'
+          month = '01'
+          year = normalize_date('year', date_group[1])
+        date = str(year)+'-'+str(month)+'-'+str(day)
+        write(line_number=str(line_number[0]), date=date)
+        # print(date)
         break
   return
 
